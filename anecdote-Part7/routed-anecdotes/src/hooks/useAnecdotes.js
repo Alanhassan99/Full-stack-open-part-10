a367477c33edbeabbab86258960494c84513855a
Alanhassan99 <alanhassan99@yahoo.com>
2026-07-28 19:39:36 +0200

    Delete anecdote-Part7/routed-anecdotes directory

--- anecdote-Part7/routed-anecdotes/src/hooks/useAnecdotes.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-import AnecdoteContext from "../anecdoteContext"
-
-const useAnecdotes = () => {
-
-  return useContext(AnecdoteContext)
-}
-
-export default useAnecdotes
